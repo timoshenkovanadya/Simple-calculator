dcbe54bccf09764811f1fdb8db1258eb48e98b09
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-09-20 14:34:26 +0300

    feat: add layout and basic functionality of theme toggle button

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,22 +1,34 @@
 import { Keyboard } from './view/Keyboard/Keyboard';
 import { Display } from './view/Display/Display';
 import { Controller } from './controller/Controller';
+import { BaseComponent } from '../shared/BaseComponent/BaseComponent';
 
 export class App {
   private appContainer: HTMLElement;
   private parent: HTMLElement;
   private keyboard: Keyboard;
   private display: Display;
+  private calculatorContainer: BaseComponent;
+  private toggleButton: BaseComponent;
 
   constructor(parent: HTMLElement) {
     this.appContainer = document.createElement('div');
-    this.appContainer.className = 'app-container';
+    this.appContainer.className = 'app-container-light';
     this.parent = parent;
-    this.display = new Display({ parentNode: this.appContainer });
-    this.keyboard = new Keyboard({ parentNode: this.appContainer });
+    this.calculatorContainer = new BaseComponent({ classNames: "calculator-container", parentNode: this.appContainer})
+    this.toggleButton = new BaseComponent({classNames: "toggle-button", tagName: "button", textContent: "theme toggle", parentNode: this.calculatorContainer.getElement()})
+    this.toggleButton.setOnclick(this.themeToggle)
+    this.display = new Display({ parentNode: this.calculatorContainer.getElement() });
+    this.keyboard = new Keyboard({ parentNode: this.calculatorContainer.getElement() });
     new Controller(this.display, this.keyboard);
   }
 
+  themeToggle = () => {
+if(this.appContainer.className === 'app-container-light') {
+  this.appContainer.classList.add("app-container-dark")
+}
+else {this.appContainer.classList.remove("app-container-dark")}
+  }
   start = () => {
     this.parent.append(this.appContainer);
   };
