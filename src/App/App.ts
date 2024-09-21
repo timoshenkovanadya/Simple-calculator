3e5276423c44810d84d9fe1a12a15ae48ead15c8
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-09-21 23:29:32 +0300

    feat: add switch theme functional

--- src/App/App.ts
+++ src/App/App.ts
@@ -16,7 +16,7 @@ export class App {
     this.appContainer.className = 'app-container-light';
     this.parent = parent;
     this.calculatorContainer = new BaseComponent({ classNames: "calculator-container", parentNode: this.appContainer})
-    this.toggleButton = new BaseComponent({classNames: "toggle-button", tagName: "button", textContent: "theme toggle", parentNode: this.calculatorContainer.getElement()})
+    this.toggleButton = new BaseComponent({classNames: "toggle-button", tagName: "button", textContent: "switch theme", parentNode: this.calculatorContainer.getElement()})
     this.toggleButton.setOnclick(this.themeToggle)
     this.display = new Display({ parentNode: this.calculatorContainer.getElement() });
     this.keyboard = new Keyboard({ parentNode: this.calculatorContainer.getElement() });
@@ -26,8 +26,17 @@ export class App {
   themeToggle = () => {
 if(this.appContainer.className === 'app-container-light') {
   this.appContainer.classList.add("app-container-dark")
+  this.calculatorContainer.setClassName("calculator-container-dark")
+  this.display.setClassName("display-dark")
+  this.keyboard.buttons.forEach((button)=> {button.setClassName("button-dark")})
+  this.toggleButton.setClassName("toggle-button-dark")
 }
-else {this.appContainer.classList.remove("app-container-dark")}
+else {this.appContainer.classList.remove("app-container-dark")
+  this.calculatorContainer.removeClassName("calculator-container-dark")
+  this.display.removeClassName("display-dark")
+  this.keyboard.buttons.forEach((button)=> {button.removeClassName("button-dark")})
+  this.toggleButton.removeClassName("toggle-button-dark")
+}
   }
   start = () => {
     this.parent.append(this.appContainer);
